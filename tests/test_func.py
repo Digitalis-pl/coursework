c5bc6c4a7644df0905769f4bbe75f78acbbc0612
georg <zhora.karsakov@mail.ru>
2024-02-01 01:22:59 +0300

    first commit

---
+++ tests/test_func.py
@@ -0,0 +1,101 @@
+from utils import get_operations
+OPERATIONS = [
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  },
+  {
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  },
+  {
+    "id": 939719570,
+    "state": "EXECUTED",
+    "date": "2018-06-30T02:08:58.425572",
+    "operationAmount": {
+      "amount": "9824.07",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 75106830613657916952",
+    "to": "Счет 11776614605963066702"
+  },
+  {
+    "id": 587085106,
+    "state": "EXECUTED",
+    "date": "2018-03-23T10:45:06.972075",
+    "operationAmount": {
+      "amount": "48223.05",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 41421565395219882431"
+  },
+  {
+    "id": 142264268,
+    "state": "EXECUTED",
+    "date": "2019-04-04T23:20:05.206878",
+    "operationAmount": {
+      "amount": "79114.93",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод со счета на счет",
+    "from": "Счет 19708645243227258542",
+    "to": "Счет 75651667383060284188"
+  },
+  {
+    "id": 873106923,
+    "state": "EXECUTED",
+    "date": "2019-03-23T01:09:46.296404",
+    "operationAmount": {
+      "amount": "43318.34",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод со счета на счет",
+    "from": "Счет 44812258784861134719",
+    "to": "Счет 74489636417521191160"
+  }]
+def test_get_operations():
+    sorted_operations = get_operations(OPERATIONS)
+    assert sorted_operations[0]["date"] == "2019-08-26T10:50:58.294041"
+    assert sorted_operations[1]["date"] == "2019-07-03T18:35:29.512364"
+    assert sorted_operations[2]["date"] == "2019-04-04T23:20:05.206878"
+    assert sorted_operations[3]["date"] == "2019-03-23T01:09:46.296404"
+    assert sorted_operations[4]["date"] == "2018-06-30T02:08:58.425572"
+    assert sorted_operations[5]["date"] == "2018-03-23T10:45:06.972075"
+
+
---
+++ utils.py
@@ -0,0 +1,86 @@
+import json
+import datetime
+def open_file():
+    with open("operations.json", "r", encoding="utf-8") as f:
+        operations_list = json.loads(f.read())
+        return operations_list
+
+
+def get_executed_operation(operations):
+    executed_operations_list = []
+    for i in operations:
+        if i == {}:
+            continue
+        if 'EXECUTED' in i['state']:
+            executed_operations_list.append(i)
+    return executed_operations_list
+
+
+def get_operations(operations: list):
+    return sorted(operations, key=lambda operation: operation["date"], reverse=True)
+
+
+def get_last_five(list):
+    return list[0:5]
+
+def get_necessary_values(d):
+    necessary_values_list = []
+    for i in d:
+        keys = ['date', 'description', 'from', 'to', 'operationAmount']
+        values1 = [i.get(key) for key in keys]
+        necessary_values_list.append(values1)
+    return necessary_values_list
+
+
+def date_format(new_date):
+    date_time_obj = datetime.datetime.strptime(new_date,'%Y-%m-%dT%H:%M:%S.%f')
+    only_date = date_time_obj.date()
+    return (only_date.strftime('%Y.%m.%d'))
+
+
+def get_currency(necessary_values_list):
+    currency_d_list = []
+    currency_list = []
+    for c in necessary_values_list:
+        currency_d_list.append(c[4])
+    for m in currency_d_list:
+        currency_list.append(read_operation_currency(m))
+    return currency_list
+
+
+def read_operation_currency(our_dict):
+    necessary_currency = []
+    necessary_currency.append(our_dict.get('currency', {}).get('name'))
+    necessary_currency.append(our_dict['amount'])
+    return necessary_currency
+
+
+def form_our_last_list(necessary_values_list, currency_list):
+    our_last_list = []
+    for i, f in zip(necessary_values_list, currency_list):
+        i.pop(4)
+        our_last_list.append(i + f)
+    return our_last_list
+
+
+def show_operations(our_last_list):
+        for i in our_last_list:
+            if i[2] == None:
+                our_date = i[0]
+                our_description = i[1]
+                our_to = i[3]
+                our_code = i[4]
+                our_amount = i[5]
+                print(f"""{date_format(our_date)} {our_description}
+{our_to[0:5]}**{our_to[21:]}
+{our_code} {our_amount}\n""")
+            else:
+                our_date = i[0]
+                our_description = i[1]
+                our_from = i[2]
+                our_to = i[3]
+                our_code = i[4]
+                our_amount = i[5]
+                print(f"""{date_format(our_date)} {our_description}
+{our_from} -> {our_to[0:5]}**{our_to[21:]}
+{our_code} {our_amount}\n""")
